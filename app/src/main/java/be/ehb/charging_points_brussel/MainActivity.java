419358836bafa7a98c0b1c7c43e9e1b44a2c0b80
VicHazewinkel <vic.hazewinkel@student.ehb.be>
2023-12-15 16:52:35 +0100

    Initial commit

---
+++ app/src/main/java/be/ehb/charging_points_brussel/MainActivity.java
@@ -0,0 +1,14 @@
+package be.ehb.charging_points_brussel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
