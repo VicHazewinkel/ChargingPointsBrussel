34eda47438904bbc3138638d81f071f83db5899d
VicHazewinkel <vic.hazewinkel@student.ehb.be>
2024-01-05 15:39:35 +0100

    Detail schermen V2 In aparte branch want werkt niet volledig zoals gewenst, zoadat master branch blijft werken

--- app/src/main/java/be/ehb/charging_points_brussel/view/DetailsFragment.java
+++ app/src/main/java/be/ehb/charging_points_brussel/view/DetailsFragment.java
@@ -31,7 +31,8 @@ public class DetailsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.id.details_fraglent, container, false);
+        // return inflater.inflate(R.id.details_fraglent, container, false);
+        return inflater.inflate(R.layout.details_fragment, container, false);
     }
 
     @Override
